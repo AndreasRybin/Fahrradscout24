2fd4277a50e063ebec78245ac90b52cf1a6b1d51
lese1016 <sebastianlenzser@gmail.com>
2019-01-05 13:29:40 +0100

    Hamburger Menu added

--- app/src/main/java/hska/fahrradscout24/MainActivity.java
+++ app/src/main/java/hska/fahrradscout24/MainActivity.java
@@ -1,13 +1,42 @@
 package hska.fahrradscout24;
 
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Toolbar toolbar;
+    //DrawerLayout drawerLayout;
+    //ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //@Override
+    //protected void onPostCreate(Bundle savedInstanceState){
+    //    super.onPostCreate(savedInstanceState);
+    //    actionBarDrawerToggle.syncState();
+    //}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void profileClick(MenuItem menuItem){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        //myIntent.putExtra("key", value); //Optional parameters
+        //String value = intent.getStringExtra("key"); //if it's a string you stored.
+        this.startActivity(intent);
     }
 }
