183f0dba1f7cb30a82bed74e0f8d4d18d9db73e1
lese1016 <sebastianlenzser@gmail.com>
2019-01-06 17:49:29 +0100

    landscape design added to profile calendar button added to profile profile picture change added to profile permissions added to manifest

--- app/src/main/java/hska/fahrradscout24/ProfileActivity.java
+++ app/src/main/java/hska/fahrradscout24/ProfileActivity.java
@@ -1,12 +1,198 @@
 package hska.fahrradscout24;
 
+import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class ProfileActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+    //load picture
+    private static int RESULT_LOAD_IMAGE = 1; //load picture
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         //getActionBar().setTitle("Hello world App");
+
+        //start calendar button
+        ImageButton selectDate = findViewById(R.id.btnDate);
+        final TextView date = findViewById(R.id.tvSelectedDate);
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ProfileActivity.this, R.style.DialogTheme,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                String strday = Integer.toString(day);
+                                String strmonth = Integer.toString(month+1);
+                                if(day<10){
+                                    strday = "0"+strday;
+                                }
+                                if(month+1 <10){
+                                    strmonth = "0"+strmonth;
+                                }
+                                date.setText(strday + "." + strmonth + "." + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.show();
+            }
+        });
+        //end calendarbutton
+
+        //load picture
+        ImageView clickLoadImage = (ImageView) findViewById(R.id.imgView);
+        clickLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if (ContextCompat.checkSelfPermission(ProfileActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    // Permission is not granted
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(ProfileActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                    } else {
+                        // No explanation needed; request the permission
+                        ActivityCompat.requestPermissions(ProfileActivity.this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                } else {
+                    // Permission has already been granted
+                    Intent i = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(i, RESULT_LOAD_IMAGE);
+                }
+
+            }
+        });
     }
+
+    //getting awnser from permission question
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent i = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    //String value = intent.getStringExtra("key"); //if it's a string you stored.
+                    startActivityForResult(i, RESULT_LOAD_IMAGE);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //getting filepath
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            //getting image from path
+            File locationOfFile = new
+                    File(Environment.getExternalStorageDirectory().getAbsolutePath()+ "/images");
+            File destination= new File(picturePath);
+            FileInputStream fileInputStream = null;
+            try {
+                fileInputStream = new FileInputStream(destination);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            //converting image to bitmap
+            Bitmap img = BitmapFactory.decodeStream(fileInputStream);
+
+            ImageView imageView = (ImageView) findViewById(R.id.imgView);
+            Bitmap picture = BitmapFactory.decodeFile(picturePath);
+            //resize bitmap
+            final int maxSize = 120;
+            int outWidth;
+            int outHeight;
+            int inWidth = picture.getWidth();
+            int inHeight = picture.getHeight();
+            if(inWidth > inHeight){
+                outWidth = maxSize;
+                outHeight = (inHeight * maxSize) / inWidth;
+            } else {
+                outHeight = maxSize;
+                outWidth = (inWidth * maxSize) / inHeight;
+            }
+
+            //Bitmap resizedBitmap = picture;
+            Bitmap resizedBitmap = Bitmap.createScaledBitmap(picture, outWidth, outHeight, true);
+            imageView.setImageBitmap(resizedBitmap);
+        }
+
+        }
+
+
+
 }
