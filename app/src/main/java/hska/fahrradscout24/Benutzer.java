614e6d133406abd2d57aef376429ab41f25d8374
baah1014 <bakir.ahmed59@gmail.com>
2019-01-10 17:33:06 +0100

    Add files via upload
    Benutzer class + dbhandler und adapter

---
+++ app/src/main/java/hska/fahrradscout24/Benutzer.java
@@ -0,0 +1,61 @@
+package com.example.bakir.database;
+
+/**
+ * Created by bakir on 10.01.2019.
+ */
+
+public class Benutzer {
+    private int benutzer_id;
+    private String benutzername;
+    private String passwort;
+    private String email;
+    private String adresse;
+
+    public Benutzer(int benutzer_id, String benutzername, String passwort, String email, String adresse) {
+        this.benutzer_id = benutzer_id;
+        this.benutzername = benutzername;
+        this.passwort = passwort;
+        this.email = email;
+        this.adresse = adresse;
+    }
+
+    public int getBenutzer_id() {
+        return benutzer_id;
+    }
+
+    public void setBenutzer_id(int benutzer_id) {
+        this.benutzer_id = benutzer_id;
+    }
+
+    public String getBenutzername() {
+        return benutzername;
+    }
+
+    public void setBenutzername(String benutzername) {
+        this.benutzername = benutzername;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+}
---
+++ app/src/main/java/hska/fahrradscout24/DbHandler.java
@@ -0,0 +1,81 @@
+package com.example.bakir.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by bakir on 07.01.2019.
+ */
+
+public class DbHandler extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "Fahrradscout";
+
+    private static final int DATABASE_VERSION = 1;
+
+    // Database creation sql statement
+    private static final String CREATE_BENUTZER_TABLE = "create table if not exists Benutzer" +
+            "(benutzer_id integer primary key AUTOINCREMENT," +
+            "benutzername text not null UNIQUE," +
+            "adresse text not null UNIQUE," +
+            "email text not null," +
+            "telefon text not null," +
+            "passwort text not null);";
+
+    private static final String CREATE_FAHRRAD_TABLE = "create table if not exists Fahrrad" +
+            "(fahrrad_id integer primary key AUTOINCREMENT," +
+            "eigentuermer integer not null," +
+            "hersteller text not null," +
+            "preis real not null," +
+            "groesse integer not null," +
+            "farbe text not null," +
+            "art_gangschaltung integer not null," +
+            "art_fahrrad integer not null," +
+            "art_bremsen integer not null);";
+    private static final String CREATE_ANZEIGE_TABLE = "create table if not exists Anzeige" +
+            "(anzeige_id integer primary key AUTOINCREMENT," +
+            "fahrrad_id integer not null," +
+            "erstelldatum text not null," +
+            "ablaufdatum text not null);";
+    private static final String CREATE_AUKTION_TABLE = "create table if not exists Auktion" +
+            "(auktion_id integer primary key AUTOINCREMENT," +
+            "fahrrad integer not null," +
+            "erstelldatum text not null," +
+            "ablaufdatum text not null," +
+            "hoechstbieter integer not null);";
+    private static final String CREATE_ART_FAHRRAD_TABLE = "create table if not exists Art_fahrrad" +
+            "(art_fahrrad_id integer primary key AUTOINCREMENT," +
+            "art text not null);";
+    private static final String CREATE_ART_BREMSEN_TABLE = "create table if not exists Art_bremsen" +
+            "(art_bremsen_id integer primary key AUTOINCREMENT," +
+            "art text not null);";
+    private static final String CREATE_ART_GANGSCHALTUNG_TABLE = "create table if not exists Art_gangschaltung" +
+            "(art_gangschaltung_id primary key AUTOINCREMENT," +
+            "art text not null);";
+
+    public DbHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(CREATE_BENUTZER_TABLE);
+        database.execSQL(CREATE_FAHRRAD_TABLE);
+        database.execSQL(CREATE_AUKTION_TABLE);
+        database.execSQL(CREATE_ANZEIGE_TABLE);
+        database.execSQL(CREATE_ART_BREMSEN_TABLE);
+        database.execSQL(CREATE_ART_FAHRRAD_TABLE);
+        database.execSQL(CREATE_ART_GANGSCHALTUNG_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database,int oldVersion,int newVersion){
+        Log.w(DbHandler.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        onCreate(database);
+    }
+}
