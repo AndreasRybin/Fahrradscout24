e9574dd91ed9b55c28826731ce3750812538d402
lese1016 <sebastianlenzser@gmail.com>
2019-01-22 19:24:54 +0100

    backstep fix on profile activity

--- app/src/main/java/hska/fahrradscout24/ProfileActivity.java
+++ app/src/main/java/hska/fahrradscout24/ProfileActivity.java
@@ -360,6 +360,19 @@ public class ProfileActivity extends AppCompatActivity {
 
         }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
 
